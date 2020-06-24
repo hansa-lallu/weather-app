23e7431c5575afe4c4b18b2621f9e2796311197c
hansa-lallu <hansalallu@gmail.com>
2020-06-24 12:28:14 +0100

    added styling, and tests

--- src/__tests__/landing.test.js
+++ src/__tests__/landing.test.js
@@ -7,9 +7,26 @@ describe('landing', () => {
     shallow(<App />);
   });
   
-  it('renders a welcome message', () => {
+  it('does not render a welcome message', () => {
     const wrapper = shallow(<App />);
     const welcome = <h2>Welcome</h2>;
-    expect(wrapper.contains(welcome)).toEqual(true);
+    expect(wrapper.contains(welcome)).toEqual(false);
+  });
+
+
+  describe('search box', () => {
+    const wrapper = shallow(<App />);
+
+    it('has a search box', () => {
+      expect(wrapper.find('input.search-bar').prop('value')).toEqual("");
+      expect(wrapper.find('input.search-bar').prop('type')).toEqual("text");
+    });
+
+    it('search field can be updated', () => {
+      wrapper.find({ className: 'search-bar'}).simulate('change', { target: { id: 'search-bar', value: 'London'} });
+      expect(wrapper.find({ className: 'search-bar'}).prop('value')).toEqual('London')
+    });
   });
 });
+
+
